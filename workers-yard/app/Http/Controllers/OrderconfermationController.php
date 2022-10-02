5da4bcba46570a9051ec8a8612cda21243cec533
dilumsadeepa <51749100+dilumsadeepa@users.noreply.github.com>
2022-10-02 20:55:23 +0530

    updated

---
+++ workers-yard/app/Http/Controllers/OrderconfermationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orderconfermation;
+use App\Http\Requests\StoreOrderconfermationRequest;
+use App\Http\Requests\UpdateOrderconfermationRequest;
+
+class OrderconfermationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreOrderconfermationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreOrderconfermationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Orderconfermation  $orderconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Orderconfermation $orderconfermation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Orderconfermation  $orderconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Orderconfermation $orderconfermation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateOrderconfermationRequest  $request
+     * @param  \App\Models\Orderconfermation  $orderconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateOrderconfermationRequest $request, Orderconfermation $orderconfermation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Orderconfermation  $orderconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Orderconfermation $orderconfermation)
+    {
+        //
+    }
+}
---
+++ workers-yard/app/Http/Controllers/PaymentconfermationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Paymentconfermation;
+use App\Http\Requests\StorePaymentconfermationRequest;
+use App\Http\Requests\UpdatePaymentconfermationRequest;
+
+class PaymentconfermationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePaymentconfermationRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePaymentconfermationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Paymentconfermation  $paymentconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Paymentconfermation $paymentconfermation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Paymentconfermation  $paymentconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Paymentconfermation $paymentconfermation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePaymentconfermationRequest  $request
+     * @param  \App\Models\Paymentconfermation  $paymentconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePaymentconfermationRequest $request, Paymentconfermation $paymentconfermation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Paymentconfermation  $paymentconfermation
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Paymentconfermation $paymentconfermation)
+    {
+        //
+    }
+}
